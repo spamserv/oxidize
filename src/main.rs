253fd66c94dec09d87406f1b29689d40e582e846
Josip Vojak <josipvojak@gmail.com>
2024-11-13 15:10:00 +0100

    Added colors to terminal.

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,26 @@ mod helpers;
 
 use blockchain::*;
 
+use colored::Colorize;
+
 fn main() {
     println!("Hello, world!");
     let mut node = Blockchain::build();
-    node.add_block();
-    node.add_block();
-    node.add_block();
-    // node.add_block();
-    // node.add_block();
+
+    println!("{}", "Creating 5 blocks".bold());
+
+    for _ in 1..=5 {
+        node.add_block();
+    }
 
     let blocks = node.blocks().clone(); 
     let third_block = blocks.get(2).unwrap().clone();
     match node.validate_single_block(third_block.header().current_hash()) {
         Err(e) => println!("Error on validating the block {} with error {}", third_block.header().current_hash(), e),
-        Ok(_) => println!("Block validated")
+        Ok(_) => println!("{}", "Block validated".bold().green())
     }
 
+    println!("{}", "Creating 2 wallets".bold());
     let mut wallet1 = Wallet::new("Wallet#1".to_string());
     wallet1.create_new_address();
     wallet1.create_new_address();
