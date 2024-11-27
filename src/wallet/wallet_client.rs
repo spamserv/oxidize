cc1501e4c9161d2203fd336ebc742ea200e7fd65
Josip Vojak <josipvojak@gmail.com>
2024-11-27 23:39:30 +0100

    Modified WalletClient to separate creating the struct and connecting to the ws server.

--- src/wallet/wallet_client.rs
+++ src/wallet/wallet_client.rs
@@ -1,26 +1,36 @@
-use std::error::Error;
 use futures_util::SinkExt;
+use std::error::Error;
 use tokio_tungstenite::tungstenite::Message;
 
-use crate::websockets::WebSocketClient;
 use super::wallet_message::WalletMessagePayload;
+use crate::websockets::WebSocketClient;
 
 #[derive(Debug, Clone)]
 pub struct WalletClient {
-    ws: WebSocketClient,
+    address: String,
+    ws: Option<WebSocketClient>,
 }
 
 impl WalletClient {
-    pub async fn new(address: String) -> Result<Self, Box<dyn Error>> {
-        let ws = WebSocketClient::new(address).await?;
-        Ok(Self { ws })
+    pub fn new(address: String) -> WalletClient {
+        Self { address, ws: None }
     }
 
-    pub async fn send_message<T: WalletMessagePayload>(&self, message: T) -> Result<(), Box<dyn Error>> {
+    pub async fn connect(&mut self) -> Result<(), Box<dyn Error>> {
+        let ws = WebSocketClient::new(self.address.to_string()).await?;
+        self.ws = Some(ws);
+        Ok(())
+    }
+
+    pub async fn send_message<T: WalletMessagePayload>(
+        &mut self,
+        message: T,
+    ) -> Result<(), Box<dyn Error>> {
         let serialized = serde_json::to_string(&message)?;
-        let mut ws_stream = self.ws.ws_stream.lock().await;
+        let mut ws_stream = self.ws.as_ref().unwrap().ws_stream.lock().await;
         // Send the message
         ws_stream.send(Message::Text(serialized)).await?;
+
         Ok(())
     }
 }
